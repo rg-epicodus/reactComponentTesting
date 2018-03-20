10e854d686d02f35eaad9e12cedbd0ad7c79b18c
Rich Garrick <richg341@gmail.com>
2018-03-20 14:43:05 -0700

    update seed component

--- src/components/Seeds/index.js
+++ src/components/Seeds/index.js
@@ -2,45 +2,45 @@ import React, { Component } from 'react';
 import './styles.css';
 // import * as firebase from 'firebase';
 
-let contacts = [{
-  name : "Scott",
-  phone : "503-555-1212"
-}, {
-  name : "Jennifer",
-  phone : "503-555-1213"
-}, {
-  name : "Juanita",
-  phone : "503-555-1214"
-}, {
-  name : "Perry",
-  phone : "503-555-1215"
-}]
+// let contacts = [{
+//   name : "Scott",
+//   phone : "503-555-1212"
+// }, {
+//   name : "Jennifer",
+//   phone : "503-555-1213"
+// }, {
+//   name : "Juanita",
+//   phone : "503-555-1214"
+// }, {
+//   name : "Perry",
+//   phone : "503-555-1215"
+// }]
 
 class Seeds extends Component {
 
-  // callApi(){
-  //   // Github fetch library : https://github.com/github/fetch
-  //   // Call the API page
-  //   fetch('http://date.jsontest.com')
-  //   .then((result) => {
-  //     // Get the result
-  //     // If we want text, call result.text()
-  //     return result.json();
-  //   }).then((jsonResult) => {
-  //     console.log(jsonResult);
-  //   })
-  // }
+  callApi(){
+    // Github fetch library : https://github.com/github/fetch
+    // Call the API page
+    fetch('http://date.jsontest.com')
+    .then((result) => {
+      // Get the result
+      // If we want text, call result.text()
+      return result.json();
+    }).then((jsonResult) => {
+      console.log(jsonResult);
+    })
+  }
 
   render() {
     return (
-      // <div>
-      //   <button onClick={() => this.callApi()}>
-      //     Click here to call API
-      //   </button>
-      // </div>
-        <li>
-          {this.props.seed.name} {this.props.seed.phone}
-        </li>
+      <div>
+        <button onClick={() => this.callApi()}>
+          Click here to call API
+        </button>
+      </div>
+        // <li>
+        //   {this.props.seed.name} {this.props.seed.phone}
+        // </li>
     );
   }
 }
